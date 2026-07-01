ec97b38a1132e8dd1a9f30c725bce259ee3e1be5
Jonghoon Ryu <jonghoon.ryu@gmail.com>
2026-07-01 13:55:02 +0900

    refactor [ exception added ]

--- baseball.cpp
+++ baseball.cpp
@@ -2,24 +2,32 @@
 #include <string>
 #include <stdexcept>
 
-class Baseball{
+class Baseball {
 public:
 	void guess(const std::string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw std::length_error("Must be 3 digits");
 		}
 
 		for (char ch : guessNumber) {
-			if ( ch < '0' || '9' < ch) {
-				throw std::invalid_argument("Must be numbers");
-			}
+			if ('0' <= ch || ch <= '9') continue;
+			throw std::invalid_argument("Must be numbers");
 		}
 
-		if ((guessNumber[0] == guessNumber[1]) 
-		|| (guessNumber[0] == guessNumber[2]) 
-		|| (guessNumber[1] == guessNumber[2]) )
-		{
+		if (isDuplicateNumber(guessNumber)) {
 			throw std::invalid_argument("Must not have the same numbers");
 		}
+	}
+
+	bool isDuplicateNumber(const std::string& guessNumber)
+	{
+		return ((guessNumber[0] == guessNumber[1])
+			|| (guessNumber[0] == guessNumber[2])
+			|| (guessNumber[1] == guessNumber[2]));
 	}
 };
